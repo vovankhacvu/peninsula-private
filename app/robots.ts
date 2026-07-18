c84a6f8b3e5df122fda082d6424e3023bfc728e4
vovankhacvu <vovankhacvu@gmail.com>
2026-07-19 01:17:53 +0700

    Fix robots sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.peninsulaprivatedn.com//sitemap.xml",
+    sitemap: "https://www.peninsulaprivatedn.com/sitemap.xml",
   };
 }
